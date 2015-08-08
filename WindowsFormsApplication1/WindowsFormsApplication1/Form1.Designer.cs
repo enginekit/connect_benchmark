98d62826ad8bd8a1472d458d2a8a1178fdaaabd7
enginekit <enginecollection@gmail.com>
2015-08-08 08:32:04 +0700

    01: init

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listboxTestCases = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listboxTestCases
+            // 
+            this.listboxTestCases.FormattingEnabled = true;
+            this.listboxTestCases.Location = new System.Drawing.Point(13, 66);
+            this.listboxTestCases.Name = "listboxTestCases";
+            this.listboxTestCases.Size = new System.Drawing.Size(373, 329);
+            this.listboxTestCases.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 445);
+            this.Controls.Add(this.listboxTestCases);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listboxTestCases;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+
+using MySqlTest;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        List<TestCase> testList = new List<TestCase>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void LoadTestCases()
+        {
+            testList.Clear();
+            listboxTestCases.Items.Clear();
+            TestCaseExtracter.ExtractTestCase(this.GetType().Assembly, testList);
+
+            //load into listbox
+            int j = testList.Count;
+            for (int i = 0; i < j; ++i)
+            {
+                listboxTestCases.Items.Add(testList[i]);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            listboxTestCases.DoubleClick += ListboxTestCases_DoubleClick;
+            LoadTestCases();
+        }
+        private void ListboxTestCases_DoubleClick(object sender, EventArgs e)
+        {
+            TestCase testCase = listboxTestCases.SelectedItem as TestCase;
+            if (testCase != null)
+            {
+                testCase.Run();
+            }
+        }
+    }
+}
